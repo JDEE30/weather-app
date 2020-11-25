5aa4e9d911727090dcd17dfc09201afbc889cdcd
Jeremy Dickey <47309416+JDEE30@users.noreply.github.com>
2020-11-24 22:56:08 -0600

    preloaded css and js files

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
 window.addEventListener('load', ()=> {
-let long;
-let lat;
-let temperatureDescription = document.querySelector('.temperature-description');
-let temperatureDegree = document.querySelector(".temperature-degree");
-let locationTimezone = document.querySelector(".location-timezone");
-let temperatureSection = document.querySelector(".temperature");
-const temperatureSpan = document.querySelector(".temperature span");
-
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(position => {
-       long = position.coords.longitude;
-       lat = position.coords.latitude;
-
-        const proxy = "https://cors-anywhere.herokuapp.com/";
-        const api = `${proxy}https://api.darksky.net/forecast/YourAPiKey/${lat},${long}`;
-
-        fetch(api)
-        .then(response =>{
-            return response.json();        
-        })
-        .then(data => {
-           const { temperature, summary, } = data.currently;
-            //set DOM ELements from the APi
-            temperatureDegree.textContent = temperature;
-            temperatureDescription.textContent = summary;
-            locationTimezone.textContent = data.timezone;
-                  });
-    });
-  } 
-});
+  let long;
+  let lat;
+  let temperatureDescription = document.querySelector('.temperature-description');
+  let temperatureDegree = document.querySelector(".temperature-degree");
+  let locationTimezone = document.querySelector(".location-timezone");
+  let temperatureSection = document.querySelector(".temperature");
+  const temperatureSpan = document.querySelector(".temperature span");
+  
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+         long = position.coords.longitude;
+         lat = position.coords.latitude;
+   
+          const proxy = "https://cors-anywhere.herokuapp.com/"; 
+          const api = `${proxy}https://api.darksky.net/forecast/0adcc61512982fc85813a13d08c097ea/${lat},${long}`;
+  
+          fetch(api)
+          .then(response =>{
+              return response.json();        
+          })
+          .then(data => {
+             const { temperature, summary, } = data.currently;
+              //set DOM ELements from the APi
+              temperatureDegree.textContent = temperature;
+              temperatureDescription.textContent = summary;
+              locationTimezone.textContent = data.timezone;
+                    });
+      });
+    } 
+  });
+  
+  new fullpage('#fullpage', {
+    autoScrolling: true,
+    navigation: true,
+  });
+  
+  
